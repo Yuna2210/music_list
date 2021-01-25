24de8abf42dd0b25931bcb42dea3deef561c096f
Yuna Kusaka <loxoo_1@yahoo.co.jp>
2021-01-25 16:13:06 +0900

    Add new link to index page

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,11 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :singer, presence: true
+
+  before_save :capitalize_name
+
+  def capitalize_name
+    self.title = self.title.capitalize
+    self.singer = self.singer.capitalize
+  end
 end
